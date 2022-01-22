5e2bcddb5913b8d80ca90021bcdcb14b55ecf5e5
elyyong <yongelynn@gmail.com>
2022-01-22 13:06:36 +1100

    hide update account unless clicked

--- static/account.js
+++ static/account.js
@@ -12,3 +12,22 @@ deleteAccount.addEventListener('click', function (event) {
         event.preventDefault();
     }
 });
+
+// to update details
+const username = document.querySelector('#to-update-name');
+
+username.addEventListener('click', function () {
+    document.querySelector('#update-name').classList.remove('hidden');
+});
+
+const email = document.querySelector('#to-update-email');
+
+email.addEventListener('click', function () {
+    document.querySelector('#update-email').classList.remove('hidden');
+});
+
+const password = document.querySelector('#to-update-password');
+
+password.addEventListener('click', function () {
+    document.querySelector('#update-password').classList.remove('hidden');
+});
